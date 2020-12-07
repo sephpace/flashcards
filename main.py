2d90738cccf23bd83a75aa9f976920d9d0e1d8dd
seph <sephpace@gmail.com>
2020-12-07 11:55:19 -0700

    Add argument parsing for command line.

--- main.py
+++ main.py
@@ -3,12 +3,49 @@ Author: Seph Pace
 Email:  sephpace@gmail.com
 """
 
-import sys
+import argparse
 
 from quiz import Quizzer
+from build import import_deck
+
+
+def build(source, output):
+    """
+    The build command. Parses a deck from a text file and saves
+    the deck into a deck file in the given output location.
+
+    Args:
+        source (str): The path to the source text file to parse.
+        output (str): The output deck file path.
+    """
+    import_deck(source, output)
+
+
+def quiz(deck):
+    """
+    Start quizzing the user with the given deck file.
+
+    Args:
+        deck (str): The path to the deck file.
+    """
+    quizzer = Quizzer(deck)
+    quizzer.start()
 
 
 if __name__ == '__main__':
-    source = sys.argv[1]
-    quizzer = Quizzer(source)
-    quizzer.start()
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=str, choices=('build', 'quiz'),
+                        help='The command to call')
+    parser.add_argument('source', type=str, help='The source file')
+    parser.add_argument('output', type=str, default='deck.yml', nargs='?',
+                        help='The output file when building a deck')
+    args = parser.parse_args()
+
+    # Run a command based on the given args
+    if args.command == 'build':
+        build(args.source, args.output)
+    elif args.command == 'quiz':
+        quiz(args.source)
+    else:
+        parser.print_usage()
